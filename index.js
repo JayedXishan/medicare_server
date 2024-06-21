c9ded75254b7d0fcad3174cca27df2c5744d8255
Jayed <jayed.jishan7@gmail.com>
2024-06-21 21:09:39 +0600

    deleted single item from user side done

--- index.js
+++ index.js
@@ -99,12 +99,12 @@ async function run() {
       res.send(result);
     })
 
-    // app.delete('/craft/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) }
-    //   const result = await craftCollection.deleteOne(query);
-    //   res.send(result);
-    // })
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // app.put('/craft/:id', async (req, res) => {
     //   const id = req.params.id;
